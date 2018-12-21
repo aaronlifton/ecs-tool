cc96a8fb079df681f2854137b95806f79849e8f7
Aaron Lifton <aaronlifton@gmail.com>
2018-12-20 17:33:47 -0800

    progress

--- main.go
+++ main.go
@@ -15,21 +15,43 @@ import (
   "os"
   "os/exec"
   "log"
-  "time"
+  "errors"
+  "io"
+  "path/filepath"
 )
+
+type EcsToolConfig struct {
+  SshUser string
+  KeyPath string
+  AlwaysEnterContainer bool
+}
+
+func readConfig(path string) (EcsToolConfig, error) {
+  data, err := ioutil.ReadFile(path)
+  if (err != nil) {
+    return EcsToolConfig{}, errors.New("Config file not found at " + path)
+  }
+  var config EcsToolConfig
+  err = json.Unmarshal(data, &config)
+  if (err != nil) {
+    return EcsToolConfig{}, errors.New("Config file has invalid json")
+  }
+  fmt.Println(config)
+  return config, nil;
+}
 
 func handleErr(err error) {
   if aerr, ok := err.(awserr.Error); ok {
-      switch aerr.Code() {
-      case ecs.ErrCodeServerException:
-          fmt.Println(ecs.ErrCodeServerException, aerr.Error())
-      case ecs.ErrCodeClientException:
-          fmt.Println(ecs.ErrCodeClientException, aerr.Error())
-      case ecs.ErrCodeInvalidParameterException:
-          fmt.Println(ecs.ErrCodeInvalidParameterException, aerr.Error())
-      default:
-          fmt.Println(aerr.Error())
-      }
+    switch aerr.Code() {
+    case ecs.ErrCodeServerException:
+        fmt.Println(ecs.ErrCodeServerException, aerr.Error())
+    case ecs.ErrCodeClientException:
+        fmt.Println(ecs.ErrCodeClientException, aerr.Error())
+    case ecs.ErrCodeInvalidParameterException:
+        fmt.Println(ecs.ErrCodeInvalidParameterException, aerr.Error())
+    default:
+        fmt.Println(aerr.Error())
+    }
   } else {
     // Print the error, cast err to awserr.Error to get the Code and
     // Message from an error.
@@ -38,6 +60,12 @@ func handleErr(err error) {
 }
 
 func main() {
+  path, _ := filepath.Abs("config.json")
+  config, err := readConfig(path)
+  if (err != nil) {
+    panic(err)
+  }
+
   sess := session.Must(session.NewSession(&aws.Config{
       Region: aws.String(endpoints.UsWest2RegionID),
   }))
@@ -57,7 +85,19 @@ func main() {
   // input2 := &ecs.ListServicesInput{Cluster: &clusterArn}
   // result2, err := svc.ListServices(input2)
 
-  serviceName := "dstld-blue-STAGING"
+  // serviceName := "dstld-blue-STAGING"
+  serviceNameErr := errors.New("You must pass the service name as the first argument.")
+
+  if (len(os.Args) < 2) {
+    handleErr(serviceNameErr)
+    return
+  }
+  serviceName := os.Args[1]
+  if (len(serviceName) == 0) {
+    handleErr(serviceNameErr)
+    return
+  }
+
   // var serviceArn string
   // for a := range result2.ServiceArns {
   //   arn := *result2.ServiceArns[a]
@@ -112,9 +152,8 @@ func main() {
   }
 
   publicIp := *result6.Reservations[0].Instances[0].PublicIpAddress
-  // fmt.Println(publicIp)
 
-
+  // Docker Web API
   resp, err := http.Get(strings.Join([]string{"http://", publicIp, ":51678/v1/tasks"},""))
   if err != nil {
     handleErr(err)
@@ -153,30 +192,41 @@ func main() {
 
   fmt.Printf("IP: %s\nDocker Id: %s\n", publicIp, dockerId)
 
-  certPath := "/Users/aaron/Downloads/consul_key.pem" // need to get absolute path from rel?
-  cmd := exec.Command("ssh", fmt.Sprintf("ec2-user@%s", publicIp), fmt.Sprintf("-i %s", certPath))
+  var cmd *exec.Cmd
+  if (config.AlwaysEnterContainer) {
+    cmd = exec.Command("ssh", "-tt",
+      fmt.Sprintf("ec2-user@%s", publicIp),
+      fmt.Sprintf("-i %s", config.KeyPath),
+      "docker exec -it "+dockerId+" /bin/bash ")
+  } else {
+    cmd = exec.Command("ssh",
+      fmt.Sprintf("ec2-user@%s", publicIp),
+      fmt.Sprintf("-i %s", config.KeyPath))
+  }
   // cmd.Env = append(cmd.Env, "TERM=xterm")
   cmd.Stdout = os.Stdout
   cmd.Stderr = os.Stderr
   cmd.Stdin = os.Stdin // Pseudo-terminal will not be allocated because stdin is not a terminal.
 
-  // r,w,_ := os.Pipe()
-  // os.Stdin = r
-  // w.WriteString(fmt.Sprintf("docker exec -it %s /bin/bash", dockerId))
-  go func() {
-    time.Sleep(11 * time.Second)
-    os.Stdin.Write([]byte("Hello\n\r"))
-  }()
-
-  // pipe
-  // w.Close()
-  // end pipe
-
-  fmt.Println(fmt.Sprintf("Executing ec2-user@%s -i %s ...", publicIp, certPath))
+  fmt.Println(fmt.Sprintf("Executing ec2-user@%s -i %s ...", publicIp, config.KeyPath))
   err = cmd.Run()
   if err != nil {
     log.Fatal(err)
   }
+
+  // cmd2 := exec.Command("source", "/etc/default/app")
+
+  stdin, err := cmd.StdinPipe()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  go func() {
+    defer stdin.Close()
+    io.WriteString(stdin, "values written to stdin are passed to cmd's standard input")
+  }()
+
+  // 2018/12/20 17:25:07 exec: Stdin already set
 
 }
 
